d6e2151fc4c6c414a0548f35b66b9e40e5a91be2
KayinJules <127335243+KayinJules@users.noreply.github.com>
2025-01-21 20:04:46 +0000

    firstcode

--- project/mySketch.js
+++ project/mySketch.js
@@ -1,8 +1,25 @@
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(500, 500, WEBGL);
 }
+let hmm = 10
+let abc = 10
 
 function draw() {
-  background(200);
+
+  rotateY(hmm/100);
+  rotateX(abc/100);
+
+  rect(-140, -150, 305, 300, 15, 150, 13, 40);
+}
+function mousePressed () {
+  if (mouseX>hmm)
+    hmm = hmm +(((mouseX+1)-(hmm+1))/20)
+  if (mouseX<hmm)
+    hmm = hmm -(((hmm+1)-(mouseX+1))/20)
+  if (mouseY>abc)
+    abc = abc +(((mouseY+1)-(abc+1))/20)
+  if (mouseY<abc)
+    abc = abc -(((abc+1)-(mouseY+1))/20)
   
 }
